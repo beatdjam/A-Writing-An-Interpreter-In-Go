5ffd03821c0d7dec2ee7775b01e2cdd1972d8229
T.Hori <beat3526@gmail.com>
2020-12-27 18:47:23 +0900

    fix ast.go

--- ast/ast.go
+++ ast/ast.go
@@ -24,6 +24,7 @@ type Program struct {
 	Statements []Statement
 }
 
+// TokenLiteral : nodeが関連付けられてるトークンのリテラル値
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
@@ -36,13 +37,14 @@ func (p *Program) TokenLiteral() string {
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
+	Value Expression
 }
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 type Identifier struct {
-	Token token.Token
+	Token token.Token // token.IDENT
 	Value string
 }
 
